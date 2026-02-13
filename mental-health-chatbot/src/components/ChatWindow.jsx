873e16c06cdfb772bc5bad1ebc681438569a9d80
Rashmika <rashmimahindrakar0025@example.com>
2026-02-13 15:08:10 +0530

    Fixed backend local call issue

--- mental-health-chatbot/src/components/ChatWindow.jsx
+++ mental-health-chatbot/src/components/ChatWindow.jsx
@@ -80,7 +80,7 @@ const ChatWindow = ({ user }) => {
     setIsTyping(true);
 
     try {
-      /* const response = await axios.post("http://127.0.0.1:8000/chat", */
+      /* const response = await axios.post("http://127.0.0.1:8000/chat",*/
       const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/chat`,
         {
           chat_id: chatId,
